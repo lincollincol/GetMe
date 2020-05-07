42cf9820197e6c97b41ead4f8c531618aeb978b5
Andrew <andriy.serb1@gmail.com>
2020-05-07 21:17:50 +0300

    Empty library

---
+++ getme/src/main/java/linc/com/getme/GetMe.kt
@@ -0,0 +1,12 @@
+package linc.com.getme
+
+import android.util.Log
+
+class GetMe {
+
+    fun execute() {
+        Log.d("GET_ME_TAG", "EXECUTE")
+        Log.d("GET_ME_TAG", "SUCCESS")
+    }
+
+}
